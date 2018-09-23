635365218ca0dba0dd745e2046190a0fd42ec8ae
Praneet Singh <praneet.singh@sjsu.edu>
2018-09-22 22:12:36 -0700

    Implement the Class class

--- lib/models/class.dart
+++ lib/models/class.dart
@@ -1,2 +1,28 @@
-///Class extends event class
+import 'package:cognito/models/event.dart';
+
+/// Class extends event class
 /// @author Praneet Singh
+
+class Class extends Event {
+  String subjectArea, courseNumber, instructor, officeLocation;
+  int units;
+  Map<DateTime, DateTime> officeHours;
+  //GradeCalculator gradeCalculator;
+  //Map<String, List<Task>> todo;
+
+  Class ({String title, String description, String location, DateTime start, DateTime end, 
+  String courseNumber, String instructor, String officeLocation, String subjectArea, int units}):
+  super(title, description, location, start, end){
+    this.courseNumber = courseNumber;
+    this.instructor = instructor;
+    this.officeLocation = officeLocation;
+    this.subjectArea = subjectArea;
+    this.units = units;
+    officeHours = Map();
+  }
+
+
+  addOfficeHours(DateTime start, DateTime end){
+        officeHours[start] = end;
+  }
+}
