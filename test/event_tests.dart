0c7d2c7f5ee4a6de6c1626bd7d4669c6ad68a65b
Julian Vu <julianvu@outlook.com>
2018-09-22 17:59:35 -0700

    Add additional Event tests

--- test/event_tests.dart
+++ test/event_tests.dart
@@ -6,5 +6,8 @@ void main() {
   test("Event Constructor Tests", () {
     Event testEvent = Event("Test Event Title", "This is a test Event object.", "Home", DateTime.now(), DateTime(2018, 12, 12));
     expect("Test Event Title", equals(testEvent.title));
+    expect("This is a test Event object.", equals(testEvent.description));
+    expect("Home", equals(testEvent.location));
+    expect(1, equals(testEvent.timeBlock.length));
   });
 }
