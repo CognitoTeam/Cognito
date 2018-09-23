839bb5f8a65927fa8b93c77b46f7525f6f64f7bf
Julian Vu <julianvu@outlook.com>
2018-09-22 18:11:49 -0700

    Add additional Event tests

--- test/event_tests.dart
+++ test/event_tests.dart
@@ -10,4 +10,22 @@ void main() {
     expect("Home", equals(testEvent.location));
     expect(1, equals(testEvent.timeBlock.length));
   });
+
+  test("Event Modification Tests", () {
+    Event testEvent = Event("Test Event Title", "This is a test Event object.", "Home", DateTime.now(), DateTime(2018, 12, 12));
+
+    testEvent.title = "Changed event title";
+    expect("Changed event title", equals(testEvent.title));
+
+    testEvent.description = "Changed event description";
+    expect("Changed event description", equals(testEvent.description));
+
+    testEvent.location = "School";
+    expect("School", testEvent.location);
+
+    testEvent.timeBlock.clear();
+    expect(0, testEvent.timeBlock.length);
+
+    testEvent.addTimeBlock(DateTime.now(), DateTime(2018, 12, 12));
+  });
 }
