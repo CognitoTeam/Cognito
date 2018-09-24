de9ddeae6c458621ca1cf6c4577480024f378912
Praneet Singh <praneet.singh@sjsu.edu>
2018-09-23 23:22:31 -0700

    Add todo implementation

--- lib/models/class.dart
+++ lib/models/class.dart
@@ -1,4 +1,7 @@
+import 'package:cognito/models/assignment.dart';
 import 'package:cognito/models/event.dart';
+import 'package:cognito/models/grade_calculator.dart';
+import 'package:cognito/models/task.dart';
 
 /// Class extends event class
 /// @author Praneet Singh
@@ -7,27 +10,75 @@ class Class extends Event {
   String subjectArea, courseNumber, instructor, officeLocation;
   int units;
   Map<DateTime, DateTime> officeHours;
-  //GradeCalculator gradeCalculator;
-  //Map<String, List<Task>> todo;
+  GradeCalculator gradeCalculator;
+  Map<String, List<Task>> todo;
 
-  Class ({String title, String description = "", String location = "", DateTime start, DateTime end, 
-  String courseNumber, String instructor, String officeLocation, String subjectArea, int units}):
-  super(
-    title: title,
-    description: description,
-    location: location,
-    start: start,
-    end: end,
-  ){
-    this.courseNumber = courseNumber;
-    this.instructor = instructor;
-    this.officeLocation = officeLocation;
-    this.subjectArea = subjectArea;
-    this.units = units;
-    officeHours = Map();
-  }
-
+  Class (
+      {String title, 
+      String description = "", 
+      String location = "", 
+      DateTime start, 
+      DateTime end, 
+      String courseNumber, 
+      String instructor, 
+      String officeLocation, 
+      String subjectArea, 
+      int units}) : super(
+  title: title,
+  description: description,
+  location: location,
+  start: start,
+  end: end,
+    ) {
+      this.courseNumber = courseNumber;
+      this.instructor = instructor;
+      this.officeLocation = officeLocation;
+      this.subjectArea = subjectArea;
+      this.units = units;
+      officeHours = Map();
+      gradeCalculator = GradeCalculator();
+      todo = Map();
+    }
+  ///
+  ///adds office hours to a class
   addOfficeHours(DateTime start, DateTime end){
         officeHours[start] = end;
+  }
+  ///
+  ///Based on the key passed in addTodoItem will add 
+  ///A new task, assignment or assessment to its 
+  ///specific List
+  addTodoItem(String key, {Task task, Assignment assignment}){
+    switch (key){
+      case "task":
+        if(todo.containsKey(key)){
+          todo[key].add(task);
+        }else{
+          List<Task> tsk = List();
+          tsk.add(task);
+          todo[key] = tsk;
+        }
+        break;
+        
+      case "assignment":
+        if(todo.containsKey(key)){
+          todo[key].add(assignment);
+        }else{
+          List<Assignment> assign = List();
+          assign.add(assignment);
+          todo[key] = assign;
+        }
+        break;
+
+      case "assessment":
+        if(todo.containsKey(key)){
+          todo[key].add(assignment);
+        }else{
+          List<Assignment> assment = List();
+          assment.add(assignment);
+          todo[key] = assment;
+        }
+        break;
+    }
   }
 }
--- test/class_tests.dart
+++ test/class_tests.dart
@@ -1,6 +1,9 @@
+import 'package:cognito/models/assignment.dart';
 ///Tester for Class class
 import 'package:test/test.dart';
 import 'package:cognito/models/class.dart';
+import 'package:cognito/models/grade_calculator.dart';
+import 'package:cognito/models/task.dart';
 
 void main(){
   test("Class Constructor Tests", (){
@@ -23,12 +26,98 @@ void main(){
     expect(testClass.instructor, equals("Test instructor"));
     expect(testClass.location, equals("Test location"));
     expect(testClass.subjectArea, equals("Computer Science"));
+    expect(testClass.units, equals(3));
+  });
 
-    testClass.officeHours.clear();
-    expect(testClass.officeHours.length, equals(0));
+  test("Test addOfficeHours", (){
+    Class testClass1 = Class(
+      title: "Test map",
+      courseNumber: "146",
+      instructor: "Test instructor",
+      officeLocation: "Test location",
+      subjectArea: "Computer Science",
+      units: 3
+    );
+    testClass1.officeHours.clear();
+    expect(testClass1.officeHours.length, equals(0));
 
-    testClass.addOfficeHours(DateTime(1997), DateTime(2018));
-    expect(testClass.officeHours.length, equals(1));
-    
+    testClass1.addOfficeHours(DateTime(1997), DateTime(2018));
+    expect(testClass1.officeHours.length, equals(1));
+  });
+
+  test("Test optional arguments constuctor", (){
+    Class testClass2 = Class(
+      title: "Test optional constructor",
+      start: DateTime.now(),
+      end: DateTime(2018, 12, 12), 
+      courseNumber: "146",
+      instructor: "Test instructor",
+      officeLocation: "San Jose",
+      subjectArea: "Computer Science",
+      units: 3
+    );
+    expect(testClass2.title, equals("Test optional constructor"));
+    expect(testClass2.description, equals(""));
+    expect(testClass2.location, equals(""));
+    expect(testClass2.courseNumber, equals("146"));
+    expect(testClass2.instructor, equals("Test instructor"));
+
+  });
+
+  test("Test todo list Assigment", (){
+    Class testClass3 = Class(
+      title: "Test optional constructor", 
+      courseNumber: "146",
+      instructor: "Test instructor",
+      officeLocation: "San Jose",
+      subjectArea: "Computer Science",
+      units: 3
+    );
+    String key = "assignment";
+    Assignment assignTest = Assignment();
+    Assignment assignTest1 = Assignment();
+    testClass3.addTodoItem(key, assignment: assignTest);
+    expect(testClass3.todo[key].length, equals(1));
+
+    testClass3.addTodoItem(key, assignment: assignTest1);
+    expect(testClass3.todo[key].length, equals(2));
+  });
+
+  test("Test todo list Task", (){
+    Class testClass4 = Class(
+      title: "Test optional constructor", 
+      courseNumber: "146",
+      instructor: "Test instructor",
+      officeLocation: "San Jose",
+      subjectArea: "Computer Science",
+      units: 3
+    );
+    String key = "task";
+    Task taskTest = Task();
+     Task taskTest1 = Task();
+    testClass4.addTodoItem(key, task: taskTest);
+    expect(testClass4.todo[key].length, equals(1));
+
+    testClass4.addTodoItem(key, task: taskTest1);
+    expect(testClass4.todo[key].length, equals(2));
+  });
+
+  test("Test todo list Assessment", (){
+    Class testClass5 = Class(
+      title: "Test optional constructor", 
+      courseNumber: "146",
+      instructor: "Test instructor",
+      officeLocation: "San Jose",
+      subjectArea: "Computer Science",
+      units: 3
+    );
+    String key = "assessment";
+    Assignment assignTest = Assignment(isAssessment: true);
+    Assignment assignTest1 = Assignment(isAssessment: true);
+    testClass5.addTodoItem(key, assignment: assignTest);
+    expect(testClass5.todo[key].length, equals(1));
+
+    testClass5.addTodoItem(key, assignment: assignTest1);
+    expect(testClass5.todo[key].length, equals(2));
   });
 }
