8c6e3fcbf51f35791088667f7a51d1aca309c700
Julian Vu <julianvu@outlook.com>
2018-11-05 08:12:43 -0800

    Implement rudimentary Class cards

--- lib/views/class_view.dart
+++ lib/views/class_view.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:cognito/models/class.dart';
 import 'package:cognito/models/academic_term.dart';
 import 'package:cognito/views/main_drawer.dart';
+import 'package:cognito/views/add_class_view.dart';
+import 'package:cognito/views/class_details_view.dart';
 
 class ClassView extends StatefulWidget {
   // Academic term object
@@ -18,6 +20,12 @@ class ClassView extends StatefulWidget {
 }
 
 class _ClassViewState extends State<ClassView> {
+  void removeClass(Class classToRemove) {
+    setState(() {
+      widget.term.classes.remove(classToRemove);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +37,64 @@ class _ClassViewState extends State<ClassView> {
         ),
         backgroundColor: Theme.of(context).primaryColorDark,
       ),
-      
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddClassView()));
+          if (result != null) {
+            widget.term.classes.add(result);
+          }
+        },
+        child: Icon(
+          Icons.add,
+          size: 42.0,
+        ),
+        backgroundColor: Theme.of(context).accentColor,
+        foregroundColor: Colors.black,
+      ),
+
+      body: widget.term.classes.isNotEmpty
+        ? ListView.builder(
+        itemCount: widget.term.classes.length,
+        itemBuilder: (BuildContext context, int index) {
+          Class classObj = widget.term.classes[index];
+
+          return Container(
+            margin: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+            child: SizedBox(
+              height: 256.0,
+
+              child: InkWell(
+                onTap: () async {
+                  await Navigator.push(context, MaterialPageRoute(builder: (context) => ClassDetailsView(classObj: classObj)));
+                },
+
+                child: Dismissible(
+                  key: Key(widget.term.classes[index].toString()),
+                  direction: DismissDirection.endToStart,
+                  onDismissed: (direction) {
+                    removeClass(classObj);
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text("${classObj.title} deleted"),
+                    ));
+                  },
+
+                  child: Card(
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          leading: Icon(Icons.label),
+                          title: Text(classObj.subjectArea + " " + classObj.courseNumber + " - " + classObj.title),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ) : null,
     );
   }
 }
