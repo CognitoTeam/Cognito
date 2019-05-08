caad3aee368df5e3168686c009f94860b76e21bd
Julian Vu <julianvu@outlook.com>
2019-05-07 20:19:11 -0700

    Fix styling in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cognito/views/login_selection_view.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
@@ -10,23 +10,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "UniPlan",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFF00227a),
         primaryColorLight: Color(0xFF6e74dc),
         primaryColorDark: Color(0xFF00227a),
         accentColor: Color(0xFFfbc02d),
         primaryTextTheme: TextTheme(
-          title: TextStyle(color: Colors.white),
-          body1: TextStyle(color: Colors.white, fontSize: 14.0),
-          body2: TextStyle(color: Colors.white70)
-        ),
-        accentTextTheme: TextTheme(
-          body1: TextStyle(color: Colors.black, fontSize: 14.0)
-        ),
+            title: TextStyle(color: Colors.white),
+            body1: TextStyle(color: Colors.white, fontSize: 14.0),
+            body2: TextStyle(color: Colors.white70)),
+        accentTextTheme:
+            TextTheme(body1: TextStyle(color: Colors.black, fontSize: 14.0)),
         inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(color: Colors.white70,),
+          hintStyle: TextStyle(
+            color: Colors.white70,
+          ),
           contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-
         ),
         hintColor: Color(0xFFfbc02d),
       ),
