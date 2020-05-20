5e263a907b1ef86912c9c65d08d2a122d0e43f0d
Jeffrey <zjeffreyl@gmail.com>
2020-05-20 16:41:43 -0700

    Finished Tasks Agenda View.

--- lib/views/AgendaViews/tasks_view.dart
+++ lib/views/AgendaViews/tasks_view.dart
@@ -1,3 +1,4 @@
+import 'package:cognito/views/components/task_agenda_item.dart';
 import 'package:flutter/material.dart';
 
 import '../main_drawer.dart';
@@ -34,29 +35,25 @@ class _TasksViewState extends State<TasksView> {
 
   Widget TasksViewBody()
   {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Text(
-            "You have 0 tasks left for today",
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        )
-        ,
-        ListView(
-          children: tasks(),
-        )
+    return ListView(
+      padding: const EdgeInsets.all(15),
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(10),
+        ),
+        SliderWidget(),
+        Padding(
+          padding: EdgeInsets.all(10),
+        ),
+        SliderWidget(),
+        Padding(
+          padding: EdgeInsets.all(10),
+        ),
+        SliderWidget(),
+        Padding(
+          padding: EdgeInsets.all(10),
+        ),
       ],
     );
   }
-
-  List<Widget> tasks()
-  {
-    
-  }
-
 }
---
+++ lib/views/components/task_agenda_item.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SliderWidget extends StatefulWidget {
+  final double sliderHeight;
+  final int min;
+  final int max;
+  final fullWidth;
+
+  SliderWidget(
+      {this.sliderHeight = 40,
+        this.max = 10,
+        this.min = 0,
+        this.fullWidth = false});
+
+  @override
+  _SliderWidgetState createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget> {
+  double _value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Color background = Theme.of(context).backgroundColor;
+
+    return Container(
+      width: this.widget.fullWidth
+          ? double.infinity
+          : (this.widget.sliderHeight) * 5.5,
+      decoration: new BoxDecoration(
+        borderRadius: new BorderRadius.all(
+          Radius.circular((this.widget.sliderHeight * .75)),
+        ),
+        gradient: new LinearGradient(
+            colors: [
+              const Color(0xFF34ACE0),
+              const Color(0xFF34ACE0),
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 1.00),
+            ),
+      ),
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.fromLTRB(20, 20, 25, 0),
+                  child: Text(
+                      "This is the a really really really long title",
+                      overflow: TextOverflow.fade,
+                      softWrap: false,
+                      style: GoogleFonts.poppins(
+                        color: Theme.of(context).backgroundColor,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )
+                  ),
+                ),
+                Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Text(
+                          "Wed, 15 April 11:59pm",
+                          overflow: TextOverflow.ellipsis,
+                          style: GoogleFonts.poppins(
+                            color: Theme.of(context).backgroundColor,
+                            fontSize: 15,
+                          )
+                      ),
+                ),
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    activeTrackColor: Colors.transparent,
+                    inactiveTrackColor: Colors.transparent,
+                    trackHeight: 4.0,
+                    thumbShape: CustomSliderThumbCircle(
+                      thumbRadius: this.widget.sliderHeight * .4,
+                      min: this.widget.min,
+                      max: this.widget.max,
+                    ),
+                    overlayColor: Colors.white.withOpacity(.4),
+                    activeTickMarkColor: background,
+                    inactiveTickMarkColor: background.withAlpha(100),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(5, 0, 5, 10),
+                    child: Slider(
+                        divisions: 10,
+                        value: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value = value;
+                          });
+                        }),
+                  )
+                ),
+              ],
+            );
+          }
+      )
+    );
+  }
+}
+
+class CustomSliderThumbCircle extends SliderComponentShape {
+  final double thumbRadius;
+  final thumbHeight;
+  final int min;
+  final int max;
+
+  const CustomSliderThumbCircle({
+    this.thumbRadius,
+    this.thumbHeight,
+    this.min,
+    this.max,
+  });
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return Size.fromRadius(thumbRadius);
+  }
+
+  @override
+  void paint(PaintingContext context, Offset center, {Animation<double> activationAnimation, Animation<double> enableAnimation, bool isDiscrete, TextPainter labelPainter, RenderBox parentBox, SliderThemeData sliderTheme, TextDirection textDirection, double value, double textScaleFactor, Size sizeWithOverflow}) {
+    final Canvas canvas = context.canvas;
+
+    final paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+    TextSpan span = new TextSpan(
+      style: new TextStyle(
+        fontSize: thumbRadius * .8,
+        fontWeight: FontWeight.w700,
+        color: sliderTheme.thumbColor,
+      ),
+      text: getValue(value),
+    );
+
+    TextPainter tp = new TextPainter(
+        text: span,
+        textAlign: TextAlign.center,
+        textDirection: TextDirection.ltr);
+    tp.layout();
+    Offset textCenter =
+    Offset(center.dx - (tp.width / 2), center.dy - (tp.height / 2));
+    canvas.drawCircle(center, thumbRadius * .9, paint);
+    tp.paint(canvas, textCenter);
+  }
+
+  String getValue(double value) {
+    return ((max * value).round()).toString();
+  }
+}
